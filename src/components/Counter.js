750b521a766b14e1476d7016fcef331bb9a23ad1
derek quinn <dereq@Troy.local>
2020-03-07 11:03:04 -0500

    handleUp, handleDown and handleReset functions added to counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,18 +3,42 @@ import './Counter.css';
 
 class Counter extends Component {
 
+    state = {
+        count: 200
+    }
+
+    handleReset = () => {
+        this.setState({ count: 0 })
+        console.log("Counter Reset")
+    }
+    handleUp = () => {
+        this.setState(prev => {
+            return { count: prev.count + 1 }
+        }
+        )
+        console.log("Counter Increse")
+    }
+    handleDown = () => {
+        this.setState(prev => {
+            return { count: prev.count - 1 }
+        }
+        )
+        console.log("Counter Decrese")
+    }
+
     render() {
 
         return (
             <div className="Counter">
-                <p>0</p>
+                <p>{this.state.count}</p>
                 <div>
-                    <button>Down</button>
-                    <button>Reset</button>
-                    <button>Up</button>
+                    <button onClick={this.handleDown}>Down</button>
+                    <button onClick={this.handleReset}>Reset</button>
+                    <button onClick={this.handleUp}>Up</button>
                 </div>
             </div>
         )
+
     }
 }
 
